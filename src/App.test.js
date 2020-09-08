d3539a83f96f9fa180eb2d70ec051705cdcf5ac7
Adam Snively <adamsnively99@gmail.com>
2020-09-08 17:35:46 -0400

    Create React APP

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders the app div', () => {
+  const { getByTestId } = render(<App />);
+  const divElement = getByTestId(/app-div/i);
+  expect(divElement).toBeInTheDocument();
 });
---
+++ src/ChatInputForm.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+export class ChatInputForm extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {message: ''};
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(event) {
+
+        this.props.onSubmit(this.state.message);
+
+        this.setState({message: ''})
+
+        event.preventDefault();
+    }
+
+    handleChange(event) {
+        this.setState({message: event.target.value});
+    }
+
+    render() {
+        return(
+            <form onSubmit={this.handleSubmit}>
+                <input type="text" name="input-message" value={this.state.message} onChange={this.handleChange}/>
+                <button type="submit" name="submit">Enter</button>
+            </form>
+        );
+    }
+}
