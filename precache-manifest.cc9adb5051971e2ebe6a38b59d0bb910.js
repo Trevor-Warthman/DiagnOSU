bcb72991b9b5a80d9e32c3a2f1f8a2a764f41dda
Trevor Warthman <trevorwarthman@Trevors-MacBook-Pro.local>
2020-09-08 19:01:31 -0400

    Updates

---
+++ precache-manifest.cc9adb5051971e2ebe6a38b59d0bb910.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bddf66241e5eb43d54a39eca2e6ce70",
+    "url": "/DiagnOSU/index.html"
+  },
+  {
+    "revision": "257be93d446513956302",
+    "url": "/DiagnOSU/static/css/main.d1d148f3.chunk.css"
+  },
+  {
+    "revision": "6c6f416c302cad19f807",
+    "url": "/DiagnOSU/static/js/2.74c953aa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/DiagnOSU/static/js/2.74c953aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "257be93d446513956302",
+    "url": "/DiagnOSU/static/js/main.ca9c3768.chunk.js"
+  },
+  {
+    "revision": "263eb46fcf3dd2b51270",
+    "url": "/DiagnOSU/static/js/runtime-main.df422ff6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/DiagnOSU/static/media/logo.5d5d9eef.svg"
+  }
+]);
