9e7f2af6197e2e4d7b0de24bde016ea387b41093
Trevor Warthman <trevorwarthman@Trevors-MacBook-Pro.local>
2020-09-17 17:44:01 -0400

    nothing

---
+++ src/LeftHandChatWindow.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+
+
+render() {
+    return (
+        <div className="left-window">
+            <h2>Important Info</h2>;
+            <p>You should know</p>;
+        </div>
+    );
+}
