e2fefc56ae1d701e7a25c30b1e361708825cb0ac
aluthe7748 <luther.80@osu.edu>
2020-09-22 18:32:53 -0400

    Remove uneccessary logo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import {ChatWindow} from "./ChatWindow";
 function App() {
   return (
     <div data-testid="app-div" className="App">
         <ChatWindow/>
-        <img src={logo} alt="React Logo"/>
     </div>
   );
 }
