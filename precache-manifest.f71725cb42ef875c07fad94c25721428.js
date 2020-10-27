9154cd6099348dc7816c7930b8495e907a115770
aluthe7748 <luther.80@osu.edu>
2020-10-27 19:21:18 -0400

    Updates

---
+++ precache-manifest.f71725cb42ef875c07fad94c25721428.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4820818b56b94554e7b0acc07c4384c0",
+    "url": "/DiagnOSU/index.html"
+  },
+  {
+    "revision": "7896c852e12341b33f99",
+    "url": "/DiagnOSU/static/css/main.8d21c800.chunk.css"
+  },
+  {
+    "revision": "c8e770647a2d270fd8fe",
+    "url": "/DiagnOSU/static/js/2.e3ba215a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/DiagnOSU/static/js/2.e3ba215a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7896c852e12341b33f99",
+    "url": "/DiagnOSU/static/js/main.745258e0.chunk.js"
+  },
+  {
+    "revision": "263eb46fcf3dd2b51270",
+    "url": "/DiagnOSU/static/js/runtime-main.df422ff6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/DiagnOSU/precache-manifest.2ce63c821ad2390ed297daf3f506cd7b.js"
+  "/DiagnOSU/precache-manifest.f71725cb42ef875c07fad94c25721428.js"
 );
 
 self.addEventListener('message', (event) => {
